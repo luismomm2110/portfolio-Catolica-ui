1de81460e6b54714a7b054288f6fa5ef713f854d
Luis Antonio Momm Duarte <luis.duarte@mercos.com>
2023-11-29 08:46:30 -0300

    retira arquivo inutil

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {useAuth} from "../auth/authProvider";
-import {ProtectedRoute} from "../auth/ProtectedRoute";
 import {PaginaInicial} from "../PaginaInicial/PaginaInicial";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
